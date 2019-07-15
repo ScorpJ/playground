37e737ff669b6fd747ef82c67166417b509dcc0c
Arthur <yangjie_5211@hotmail.com>
2019-07-15 23:15:41 +0800

    commit 1

--- java/spring-could-study/api-gateway-scg/src/main/java/org/springframework/cloud/gateway/filter/LoadBalancerClientFilterImpl.java
+++
@@ -1,9 +0,0 @@
-package org.springframework.cloud.gateway.filter;
-
-import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
-
-public class LoadBalancerClientFilterImpl extends LoadBalancerClientFilter {
-    public LoadBalancerClientFilterImpl(LoadBalancerClient loadBalancer) {
-        super(loadBalancer);
-    }
-}
