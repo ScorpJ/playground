5f90cd262d2202590f0c4a2396a1fc9c0e19e5f3
Arthur <yangjie_5211@hotmail.com>
2019-08-18 23:44:56 +0800

    CompletableFuture test

--- java/javabasic/src/test/java/com/arthur/basic/j8f/CompletableFutureTest.java
+++ java/javabasic/src/test/java/com/arthur/basic/j8f/CompletableFutureTest.java
@@ -11,25 +11,30 @@ public class CompletableFutureTest {
 
         final List<String> list = Arrays.asList("Hello","World", "Hi", "CompletableFuture");
         final List<String> list2 = Arrays.asList("hello","world", "hi", "completablefuture");
-        final CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(() -> {
-             System.out.println(list.get(0));
-             return list.get(0);
+        final CompletableFuture<Void> completableFuture = CompletableFuture.supplyAsync(() -> {
+             System.out.println("Process for list 1");
+             return null;
         });
 
-        list.stream().skip(1).forEach(e -> completableFuture.thenRun(() -> System.out.println(e)));
+        list.stream().forEach(e -> completableFuture.thenRun(() -> System.out.println(e)));
 
 
-        final CompletableFuture<String> completableFuture2 = CompletableFuture.supplyAsync(() -> {
-            System.out.println(list2.get(0));
-            return list2.get(0);
+        final CompletableFuture<Void> completableFuture2 = CompletableFuture.supplyAsync(() -> {
+            System.out.println("Process for list 2");
+            return null;
         });
 
-        list2.stream().skip(1).forEach(e -> completableFuture2.thenRun(() -> System.out.println(e)));
+        list2.stream().forEach(e -> completableFuture2.thenRun(() -> System.out.println(e)));
 
         CompletableFuture.allOf(completableFuture,completableFuture).join();
 
 
         System.out.println("Done");
+
+//        CompletableFuture<Void> cf = CompletableFuture.supplyAsync(() -> {
+//            System.out.println(list.get(0));
+//            return null;
+//        });
 
     }
 }
